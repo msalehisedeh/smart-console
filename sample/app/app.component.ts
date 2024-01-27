a3291d00724fed6c912144fa9471d626f1dfd1f2
Masoud Salehisedeh <msalehi@astound.net>
2024-01-27 13:15:43 -0800

    documentation update

--- sample/app/app.component.ts
+++ sample/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
-import { SmartConsoleService } from '@sedek/smart-console';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SmartConsoleService } from '@sedeh/smart-console';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  providers: [SmartConsoleService],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Smart Console';
@@ -33,7 +37,7 @@ export class AppComponent {
     blockCaller: 'core.js,Zone.ts,MyComponent.ts',
     suppress: 'test3,test4',
     filter: '',
-    watch: 'sanitizing'
+    watch: 'sanitizing',
   };
 
   constructor(
@@ -42,13 +46,11 @@ export class AppComponent {
   ) {
     this.smartService.makeSmartLogs({
       emitOutput: true,
-      upscale: true
+      upscale: true,
     });
-    this.smartService.redirectedOutput().subscribe(
-      (event) => {
-        this.data.push(this.smartService.markupTrace(event));
-      }
-    );
+    this.smartService.redirectedOutput().subscribe((event) => {
+      this.data.push(this.smartService.markupTrace(event));
+    });
   }
 
   update(item: string, event: any) {
@@ -64,7 +66,7 @@ export class AppComponent {
     console.clear();
   }
   onChange(event: any) {
-    this.options.throttleOn = parseInt(event.target.value,10)
+    this.options.throttleOn = parseInt(event.target.value, 10);
   }
 
   execute() {
@@ -88,13 +90,13 @@ export class AppComponent {
     if (this.options.traceDisabled) {
       newOptions['traceDisabled'] = true;
     }
-    if (this.options.assertDisabled){
+    if (this.options.assertDisabled) {
       newOptions['assertDisabled'] = true;
     }
-    if (this.options.debugDisabled){
+    if (this.options.debugDisabled) {
       newOptions['debugDisabled'] = true;
     }
-    if (this.options.exceptionDisabled){
+    if (this.options.exceptionDisabled) {
       newOptions['exceptionDisabled'] = true;
     }
     if (this.options.downGrade) {
@@ -112,7 +114,7 @@ export class AppComponent {
     if (this.options.logAfterEmit) {
       newOptions['logAfterEmit'] = true;
     }
-    if(this.options.throttleOn) {
+    if (this.options.throttleOn) {
       newOptions['throttleOn'] = this.options.throttleOn;
     }
     if (this.options.blockCaller) {
@@ -129,66 +131,72 @@ export class AppComponent {
       const list = this.options.watch.split(',');
       this.smartService.clearWatchList(this.subscribers);
       this.subscribers = [];
-      list.map(
-        (key: any) => {
-          const sbc = this.smartService.addWatch(key).subscribe(
-            (event: any) => {
-              this.data.push("Watch for '" + key +
-                            "' is reporting log:" +
-                            (typeof event==='object' ? JSON.stringify(event) : event) 
-                        );
-            }
+      list.map((key: any) => {
+        const sbc = this.smartService.addWatch(key).subscribe((event: any) => {
+          this.data.push(
+            "Watch for '" +
+              key +
+              "' is reporting log:" +
+              (typeof event === 'object' ? JSON.stringify(event) : event)
           );
-          this.subscribers.push(sbc);
-        }
-      );
+        });
+        this.subscribers.push(sbc);
+      });
     } else {
       this.smartService.clearWatchList(this.subscribers);
       this.subscribers = [];
     }
 
-    console.log("test","1","2");
-    console.info("test","1",{'x': 1, 'y': 'sanitizing', 'z': []});
-    console.info("test2","1","2");
-    console.warn("test","1","2");
-    console.warn("test3","1","2");
-    console.error("test","1","2");
-    console.error("test4","1","2");
-    console.debug('debug this bug',"1,2");
-    console.table(["test","1","2"]);
-    console.log('level_3', 'message that may not be as imortant', 'additional info');
+    console.log('test', '1', '2');
+    console.info('test', '1', { x: 1, y: 'sanitizing', z: [] });
+    console.info('test2', '1', '2');
+    console.warn('test', '1', '2');
+    console.warn('test3', '1', '2');
+    console.error('test', '1', '2');
+    console.error('test4', '1', '2');
+    console.debug('debug this bug', '1,2');
+    console.table(['test', '1', '2']);
+    console.log(
+      'level_3',
+      'message that may not be as imortant',
+      'additional info'
+    );
     console.log('level_6', 'message that may be imortant', 'additional info');
     console.info('level_6', 'message that may be imortant', 'additional info');
-    console.trace('level_5', 'message that may or may not be as imortant', 'additional info');
-    console.warn('level_5', 'message that may or may not be as imortant', 'additional info');
-  
-    this._trace();
-    console.assert(false, "asserting test!");
-    this.http.get("https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&format=json&api_key=API_KEY&user_id=USER_ID").subscribe(
-      (success) => {
-
-      },
-      (error) => {
-
-      }
+    console.trace(
+      'level_5',
+      'message that may or may not be as imortant',
+      'additional info'
     );
-    this.http.get("https://something.org").subscribe(
-      (success) => {
+    console.warn(
+      'level_5',
+      'message that may or may not be as imortant',
+      'additional info'
+    );
 
-      },
-      (error) => {
-
-      }
-    )
+    this._trace();
+    console.assert(false, 'asserting test!');
+    this.http
+      .get(
+        'https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&format=json&api_key=API_KEY&user_id=USER_ID'
+      )
+      .subscribe(
+        (success) => {},
+        (error) => {}
+      );
+    this.http.get('https://something.org').subscribe(
+      (success) => {},
+      (error) => {}
+    );
   }
-	private _trace() {
-		// this method purpose is only to fix IE issue. 
-		// in IE, new Error().stack  will be undefined
-		// unless it is caugth in try block statement.
-		try {
-		  throw new Error('testing trace log');
-		} catch(e: any) {
+  private _trace() {
+    // this method purpose is only to fix IE issue.
+    // in IE, new Error().stack  will be undefined
+    // unless it is caugth in try block statement.
+    try {
+      throw new Error('testing trace log');
+    } catch (e: any) {
       console.trace(e.stack);
-		}
-	  }
+    }
+  }
 }
